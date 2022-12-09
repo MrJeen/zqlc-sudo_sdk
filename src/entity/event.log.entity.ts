24db4b5bd3aea796aae19251dd230849cf807ac0
d.jy <jy20220520@gmail.com>
2022-12-09 15:03:58 +0800

    添加事件记录

---
+++ src/entity/event.log.entity.ts
@@ -0,0 +1,44 @@
+import { Column, Entity, Index } from 'typeorm';
+import { BaseEntity } from './base.entity';
+
+@Entity('event_logs')
+@Index(['token_address'])
+@Index(['pool_address'])
+@Index(['chain_id', 'transaction_hash', 'log_index'], { unique: true })
+export class EventLogEntity extends BaseEntity {
+  @Column('int', { default: 0, comment: '区块链id' })
+  chain_id;
+
+  @Column('varchar', { default: '', comment: '池子地址' })
+  pool_address;
+
+  @Column('varchar', { default: '', comment: '系列地址' })
+  token_address;
+
+  @Column('varchar', { default: '', comment: '币种地址' })
+  currency;
+
+  @Column('int2', {
+    default: 0,
+    comment: '池子类型（0: 买池，1: 卖池，2:流动池）',
+  })
+  pool_type;
+
+  @Column('timestamp', { default: () => 'NOW()', comment: '订单创建时间' })
+  block_time;
+
+  @Column('int', { default: 0, comment: '交易区块' })
+  block_number;
+
+  @Column('varchar', { default: '', comment: '交易哈希' })
+  transaction_hash;
+
+  @Column('int', { default: 0, comment: '日志索引值' })
+  log_index;
+
+  @Column('varchar', { default: '', comment: '事件名称' })
+  event_name;
+
+  @Column('json', { default: {}, comment: '明细' })
+  info;
+}
