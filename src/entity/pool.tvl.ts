0e13845a4bbadc57beeef20e67968b9200260966
d.jy <jy20220520@gmail.com>
2022-12-13 10:38:43 +0800

    新增表

---
+++ src/entity/pool.tvl.ts
@@ -0,0 +1,30 @@
+import { Column, Entity, Index } from 'typeorm';
+import { BaseEntity } from './base.entity';
+
+/**
+ * 所有买池（包括流动池）的余额变化
+ */
+@Entity('pool_tvl')
+@Index(['date'])
+@Index(['chain_id', 'token_address'])
+export class PoolTvlEntity extends BaseEntity {
+  @Column('int', { default: 0, comment: '区块链id' })
+  chain_id;
+
+  @Column('varchar', { default: '', comment: '系列地址' })
+  token_address;
+
+  @Column('varchar', { default: '', comment: '池子地址' })
+  pool_address;
+
+  @Column('varchar', { default: '', comment: '日期' })
+  date;
+
+  @Column('decimal', {
+    precision: 36,
+    scale: 20,
+    default: 0,
+    comment: '余额',
+  })
+  tvl;
+}
