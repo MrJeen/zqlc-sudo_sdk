33fc29328f81fbb0170c2520a87df8e364d6ecee
d.jy <jy20220520@gmail.com>
2023-01-10 17:29:20 +0800

    fix

---
+++ src/entity/openmeta/index.ts
@@ -0,0 +1,4 @@
+export * from './collection.entity';
+export * from './nft.entity';
+export * from './nft.owner.entity';
+export * from './rank.entity';
---
+++ src/utils/index.ts
@@ -0,0 +1,4 @@
+export * from './dingtalk.util';
+export * from './ethers.util';
+export * from './helper.util';
+export * from './logger.util';
