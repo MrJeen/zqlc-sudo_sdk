81204d3ae35b750527f53d42d32d7ecbe54a05a9
d.jy <jy20220520@gmail.com>
2023-03-29 19:12:56 +0800

    优化

--- src/decorator/custom.decorator.ts
+++ src/decorator/custom.decorator.ts
@@ -3,26 +3,38 @@ import _ from 'lodash';
 import BigNumber from 'bignumber.js';
 
 // 移除小数点后面多余的0，小数点后面只有0时，移除小数
-export function FormatNumber() {
-  return Transform(({ value }) => (value ? BigNumber(value).toFixed() : value));
+export function FormatNumber(options = null) {
+  return Transform(
+    ({ value }) => (value ? BigNumber(value).toFixed() : value),
+    options ?? {},
+  );
 }
 
 // 转为小写字母
-export function ToLowerCase() {
-  return Transform(({ value }) => (value ? value.toLowerCase() : value));
+export function ToLowerCase(options = null) {
+  return Transform(
+    ({ value }) => (value ? value.toLowerCase() : value),
+    options ?? {},
+  );
 }
 
 // 转为大写字母
-export function ToUpperCase() {
-  return Transform(({ value }) => (value ? value.toUpperCase() : value));
+export function ToUpperCase(options = null) {
+  return Transform(
+    ({ value }) => (value ? value.toUpperCase() : value),
+    options ?? {},
+  );
 }
 
 // 移除字符串两端空格或指定字符
-export function Trim(char = ' ') {
-  return Transform(({ value }) => (value ? _.trim(value, char) : value));
+export function Trim(char = ' ', options = null) {
+  return Transform(
+    ({ value }) => (value ? _.trim(value, char) : value),
+    options ?? {},
+  );
 }
 
 // 复制其他键值
-export function Duplicate(key) {
-  return Transform(({ value, obj }) => value ?? obj[key]);
+export function Duplicate(key: string, options = null) {
+  return Transform(({ value, obj }) => value ?? obj[key], options ?? {});
 }
