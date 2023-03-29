4312f1ec2256b7d11ae7e32367201a0f71502462
d.jy <jy20220520@gmail.com>
2023-03-29 19:17:50 +0800

    优化

--- src/decorator/custom.decorator.ts
+++ src/decorator/custom.decorator.ts
@@ -1,40 +1,40 @@
-import { Transform } from 'class-transformer';
+import { Transform, TransformOptions } from 'class-transformer';
 import _ from 'lodash';
 import BigNumber from 'bignumber.js';
 
 // 移除小数点后面多余的0，小数点后面只有0时，移除小数
-export function FormatNumber(options = null) {
+export function FormatNumber(options?: TransformOptions) {
   return Transform(
     ({ value }) => (value ? BigNumber(value).toFixed() : value),
-    options ?? {},
+    options,
   );
 }
 
 // 转为小写字母
-export function ToLowerCase(options = null) {
+export function ToLowerCase(options?: TransformOptions) {
   return Transform(
     ({ value }) => (value ? value.toLowerCase() : value),
-    options ?? {},
+    options,
   );
 }
 
 // 转为大写字母
-export function ToUpperCase(options = null) {
+export function ToUpperCase(options?: TransformOptions) {
   return Transform(
     ({ value }) => (value ? value.toUpperCase() : value),
-    options ?? {},
+    options,
   );
 }
 
 // 移除字符串两端空格或指定字符
-export function Trim(char = ' ', options = null) {
+export function Trim(char = ' ', options?: TransformOptions) {
   return Transform(
     ({ value }) => (value ? _.trim(value, char) : value),
-    options ?? {},
+    options,
   );
 }
 
 // 复制其他键值
-export function Duplicate(key: string, options = null) {
-  return Transform(({ value, obj }) => value ?? obj[key], options ?? {});
+export function Duplicate(key: string, options?: TransformOptions) {
+  return Transform(({ value, obj }) => value ?? obj[key], options);
 }
